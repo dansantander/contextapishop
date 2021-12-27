c5fe0c3f49ee1a64485c238a1344425b833f8e6f
dansantander <dansantandr@gmail.com>
2021-12-27 16:08:49 -0600

    chore: add collapsable searchbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Form, FormControl, Nav, Navbar, Dropdown } from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand>
-            Logo
-        </Navbar.Brand>
+        <Navbar.Brand>Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll" className="justify-content-end">
+          <Form className="d-flex">
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+          </Form>
+          <Nav
+            className="my-2 my-lg-0"
+            navbarScroll
+          >
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              1
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   )
