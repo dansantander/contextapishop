1539d94e7b914b02db448ceff83d1e79b655e3b9
dansantander <dansantandr@gmail.com>
2022-01-09 12:26:52 -0600

    chore: add total amount in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Button, ListGroup } from "react-bootstrap";
+import { CartState } from "../context/Context";
 
 const Cart = () => {
-  return (
-    <div>
-      This is the Cart page
-    </div>
-  )
-}
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
 
-export default Cart
+  const [total, setTotal] = useState();
+
+  useEffect(() => {
+    setTotal(cart.reduce((acc, curr) =>  acc + Number(curr.price)*curr.qty, 0))
+  }, [cart])
+
+  return (
+    <div className="home">
+      <div className="productContainer">
+        <ListGroup>
+          {cart.map((p) => (
+            <span>{p.name}</span>
+          ))}
+          <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div className="filterContainer summary">
+        <span className="title">Subtotal ({cart.length}) items</span>
+        <span style={{ fontWeight: 700, fontSize: 20 }}>Total: $ {total}</span>
+        <Button type="button" disabled={cart.length === 0}>
+          Proceed to checkout
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import { FaShoppingCart } from 'react-icons/fa';
-import { Container, Form, FormControl, Nav, Navbar, Dropdown, Badge, Button } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import { CartState } from '../context/Context';
-import { AiFillDelete } from 'react-icons/ai';
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import {
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  Dropdown,
+  Badge,
+  Button,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { CartState } from "../context/Context";
+import { AiFillDelete } from "react-icons/ai";
 
 const Header = () => {
-
   const {
     state: { cart },
-    dispatch
+    dispatch,
   } = CartState();
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand><Link to="/">LOGO</Link></Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/">LOGO</Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll" className="justify-content-end">
           <Form className="d-flex">
@@ -26,58 +36,55 @@ const Header = () => {
               aria-label="Search"
             />
           </Form>
-          <Nav
-            className="my-2 my-lg-0"
-            navbarScroll
-          >
-          <Dropdown alignRight>
-            <Dropdown.Toggle variant="success" >
-              <FaShoppingCart color="white" fontSize="25px"/>
-              <Badge>{cart.length}</Badge>
-            </Dropdown.Toggle>
+          <Nav className="my-2 my-lg-0" navbarScroll>
+            <Dropdown alignRight>
+              <Dropdown.Toggle variant="success">
+                <FaShoppingCart color="white" fontSize="25px" />
+                <Badge>{cart.length}</Badge>
+              </Dropdown.Toggle>
 
-            <Dropdown.Menu style={{ minWidth: 370 }}>
-              { cart.length > 0 ? (
-              <>
-              {
-                cart.map((p) => (
-                  <span className="cartItem" key={p.id}>
-                    <img
-                      src={p.image}
-                      className="cartItemImg"
-                      alt={p.name}  
-                    />
-                    <div className="cartItemDetail">
-                      <span>{p.name}</span>
-                      <span>$ {p.price}</span>
-                    </div>
-                    <AiFillDelete
-                      fontSize="20px"
-                      style={{cursor:"pointer"}}
-                      onClick={()=>
-                        dispatch({
-                          type: "REMOVE_FROM_CART",
-                          payload: p
-                        })
-                      }
-                    />
-                  </span>
-                ))
-              }
-              <Link to="/cart">
-                    <Button style={{ width: "95%", margin: "0 10px" }}>
-                      Go To Cart
-                    </Button>
-                  </Link>
-              </>):(
-                <Dropdown.Item>Cart is Empty</Dropdown.Item>) }
-            </Dropdown.Menu>
-          </Dropdown>
+              <Dropdown.Menu style={{ minWidth: 370 }}>
+                {cart.length > 0 ? (
+                  <>
+                    {cart.map((p) => (
+                      <span className="cartItem" key={p.id}>
+                        <img
+                          src={p.image}
+                          className="cartItemImg"
+                          alt={p.name}
+                        />
+                        <div className="cartItemDetail">
+                          <span>{p.name}</span>
+                          <span>$ {p.price}</span>
+                        </div>
+                        <AiFillDelete
+                          fontSize="20px"
+                          style={{ cursor: "pointer" }}
+                          onClick={() =>
+                            dispatch({
+                              type: "REMOVE_FROM_CART",
+                              payload: p,
+                            })
+                          }
+                        />
+                      </span>
+                    ))}
+                    <Link to="/cart">
+                      <Button style={{ width: "95%", margin: "0 10px" }}>
+                        Go To Cart
+                      </Button>
+                    </Link>
+                  </>
+                ) : (
+                  <Dropdown.Item>Cart is Empty</Dropdown.Item>
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
